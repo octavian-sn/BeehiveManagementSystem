5842ae0176fd0d6cd6bef9db2d150dc7c2b35d2f
Șulic Octavian <octavius1992@gmail.com>
2023-07-09 18:27:11 +0300

    Add "Bee" class and "EggCare", "NectarCollector", "HoneyManufacturer" subclasses.

---
+++ BeehiveManagementSystem/Bee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeehiveManagementSystem
+{
+    public class Bee
+    {
+        public virtual float CostPerShift { get; }
+        public string Job { get; }
+        public Bee(string job)
+        {
+            Job = job;
+        }
+        public void WorkTheNextShift()
+        {
+          if (HoneyVault.ConsumeHoney(CostPerShift)) DoJob();
+        }
+        protected virtual void DoJob() { /* Will be over-written by subclass */ }
+    }
+}
---
+++ BeehiveManagementSystem/EggCare.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeehiveManagementSystem
+{
+    public class EggCare : Bee
+    {
+        public EggCare (string job) : base (job) { }
+    }
+}
